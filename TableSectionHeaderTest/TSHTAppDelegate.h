84cb458b5ef893d71d6f92bcd04fd9c4ef641847
Andrew Wilcox <AWilcox@Wilcox-Tech.com>
2012-05-29 13:03:15 -0400

    Initial Commit

---
+++ TableSectionHeaderTest/TSHTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TSHTAppDelegate.h
+//  TableSectionHeaderTest
+//
+//  Created by Andrew Wilcox on 29/05/2012.
+//  Copyright (c) 2012 Wilcox Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSHTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableSectionHeaderTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableSectionHeaderTest
+//
+//  Created by Andrew Wilcox on 29/05/2012.
+//  Copyright (c) 2012 Wilcox Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TSHTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([TSHTAppDelegate class]));
+	}
+}
